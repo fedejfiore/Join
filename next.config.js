5fc0fca5f5b8394b3a11cb9af93a431cf48e8a6a
fedejfiore <77334697+fedejfiore@users.noreply.github.com>
2026-06-12 21:52:58 -0300

    fix: restaura next15 y aplica mock de __dirname para el edge runtime

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
-  // Aquí también podrías poner las redirecciones WWW si no las haces por Vercel
+  
+  // SOLUCCIÓN QUIRÚRGICA: Neutraliza el ReferenceError en el Edge Runtime de Vercel
+  webpack: (config, { isServer, nextRuntime }) => {
+    if (isServer && nextRuntime === 'edge') {
+      config.node = {
+        __dirname: true, // Le provee un contexto seguro de __dirname al bundle del middleware
+      };
+    }
+    return config;
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
